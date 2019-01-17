2378721aa4b44d93bd112e5ed23506a5840e7bd8
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-01-17 20:18:04 -0300

    chore(api): enable cors middleware

--- api/config/app.js
+++ api/config/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import routes from './routes';
 import { handleErrors } from '../middlewares/errors';
@@ -8,6 +9,7 @@ import { handleErrors } from '../middlewares/errors';
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 
 app.use('/', routes);
 
