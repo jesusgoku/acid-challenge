ebe90055db05504cbe22ef87e06828a5b1df32ec
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-01-17 01:03:21 -0300

    chore(api): enable helmet middleware

--- api/config/app.js
+++ api/config/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import helmet from 'helmet';
 
 import routes from './routes';
 import { handleErrors } from '../middlewares/errors';
 
 
 const app = express();
+
+app.use(helmet());
 
 app.use('/', routes);
 
