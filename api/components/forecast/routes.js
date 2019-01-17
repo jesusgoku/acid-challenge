e1166ebf4ea9b8cce5889335d77e74189eabc53f
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-01-17 03:48:55 -0300

    feat(api): enable cache middleware

--- api/components/forecast/routes.js
+++ api/components/forecast/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 import { getForecast, getTimeMachine } from './controllers';
+import { forecastCache } from '../../middlewares/cache';
 
 
 const router = express.Router();
 
-router.get('/:lat,:lng', getForecast);
+router.get('/:lat,:lng', forecastCache, getForecast);
 router.get('/:lat,:lng,:time', getTimeMachine);
 
 export default router;
