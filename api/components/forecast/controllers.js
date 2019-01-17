5826663ec8e746de0d950ffd21a0eb8b724ef765
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-01-17 00:49:06 -0300

    feat(api): propage error to next middleware

--- api/components/forecast/controllers.js
+++ api/components/forecast/controllers.js
@@ -1,20 +1,22 @@
 import forecast from '../../services/forecast';
 
 
-export function getForecast(req, res) {
+export function getForecast(req, res, next) {
   const { lat, lng } = req.params;
 
   forecast
     .getCurrentForecast(lat, lng)
     .then(data => res.json(data))
+    .catch(next)
   ;
 }
 
-export function getTimeMachine(req, res) {
+export function getTimeMachine(req, res, next) {
   const { lat, lng, time } = req.params;
 
   forecast
     .getTimeMachine(lat, lng, time)
     .then(data => res.json(data))
+    .catch(next)
   ;
 }
