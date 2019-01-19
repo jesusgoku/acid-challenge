3727454acf375edddb996651bad83fd4237f0015
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-01-19 20:55:37 -0300

    feat(app): added forecast api library

---
+++ app/src/libraries/forecast.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+export default class Forecast {
+  constructor() {
+    this.baseURL = process.env.REACT_APP_FORECAST_API_BASE_URL || 'http://localhost:8000';
+  }
+
+  request(options) {
+    const {
+      url,
+      params = {},
+      headers = {},
+      ...restOptions
+    } = options;
+
+    const requestOptions = {
+      url,
+      params,
+      headers,
+      baseURL: `${this.baseURL}`,
+      ...restOptions,
+    };
+
+    return axios(requestOptions)
+      .then(res => res.data)
+    ;
+  }
+
+  getCurrentForecast(lat, lng) {
+    return this.request({
+      url: `/forecast/${lat},${lng}`,
+    });
+  }
+}
