9e65512c2056f8d4cc43e6b8090a3113624d13cd
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-01-19 21:07:17 -0300

    feat(app): added posibility to display icon

--- app/src/components/ForecastBox.js
+++ app/src/components/ForecastBox.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 
 import './ForecastBox.css';
 
-function ForecastBox({ country, capital, temperature }) {
+function ForecastBox({ country, capital, temperature, icon }) {
   return (<div className="ForecastBox">
     <p className="ForecastBox__country">{ country }</p>
     <p className="ForecastBox__capital">{ capital }</p>
     <p className="ForecastBox__temperature">{ temperature }º C</p>
+    { icon && <i className={`ForecastBox__icon wi wi-forecast-io-${icon}`}></i>}
   </div>);
 }
 
@@ -15,6 +16,7 @@ ForecastBox.propTypes = {
   country: PropTypes.string.isRequired,
   capital: PropTypes.string.isRequired,
   temperature: PropTypes.number.isRequired,
+  icon: PropTypes.string,
 };
 
 export default ForecastBox;
