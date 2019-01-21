ad03d71bb6193dbacd0d1c7b4e04363f2cbbe9da
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-01-21 00:46:57 -0300

    chore(app): remove default value to api url

--- app/src/libraries/forecast.js
+++ app/src/libraries/forecast.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class Forecast {
   constructor() {
-    this.baseURL = process.env.REACT_APP_FORECAST_API_BASE_URL || 'http://localhost:8000';
+    this.baseURL = process.env.REACT_APP_FORECAST_API_BASE_URL;
   }
 
   request(options) {
